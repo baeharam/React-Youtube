3ab79f55cd852078c909e1fee28b839860329209
Haram Bae <hisfedev@gmail.com>
2020-01-07 22:46:11 +0900

    Complement sidebar
    Signed-off-by: Haram Bae <hisfedev@gmail.com>

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,30 +1,67 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faFire } from'@fortawesome/free-solid-svg-icons';
+import { 
+  faHome, 
+  faFire, 
+  faFileVideo,
+  faHistory,
+  faClock,
+  faThumbsUp 
+} from'@fortawesome/free-solid-svg-icons';
 import subscription from '../../assets/youtube-subscription.png';
 import './Sidebar.scss';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <button className="sidebar__item">
-        <div className="sidebar__item-icon-wrapper">
-          <FontAwesomeIcon className="sidebar__item-icon" icon={faHome} />
-        </div>
-        <span className="sidebar__item-text">홈</span>
-      </button>
-      <button className="sidebar__item">
-        <div className="sidebar__item-icon-wrapper">
-          <FontAwesomeIcon className="sidebar__item-icon" icon={faFire} />
-        </div>
-        <span className="sidebar__item-text">인기</span>
-      </button>
-      <button className="sidebar__item">
-        <div className="sidebar__item-icon-wrapper">
-          <img className="sidebar__item-img" src={subscription} alt="구독"/>
-        </div>
-        <span className="sidebar__item-text">구독</span>
-      </button>
+      <div className="sidebar__item-wrapper">
+        <button className="sidebar__item">
+          <div className="sidebar__item-icon-wrapper">
+            <FontAwesomeIcon className="sidebar__item-icon" icon={faHome} />
+          </div>
+          <span className="sidebar__item-text">홈</span>
+        </button>
+        <button className="sidebar__item">
+          <div className="sidebar__item-icon-wrapper">
+            <FontAwesomeIcon className="sidebar__item-icon" icon={faFire} />
+          </div>
+          <span className="sidebar__item-text">인기</span>
+        </button>
+        <button className="sidebar__item">
+          <div className="sidebar__item-icon-wrapper">
+            <img className="sidebar__item-img" src={subscription} alt="구독"/>
+          </div>
+          <span className="sidebar__item-text">구독</span>
+        </button>
+      </div>
+      <div className="sidebar__line"></div>
+      <div className="sidebar__item-wrapper">
+        <button className="sidebar__item">
+          <div className="sidebar__item-icon-wrapper">
+            <FontAwesomeIcon className="sidebar__item-icon" icon={faFileVideo} />
+          </div>
+          <span className="sidebar__item-text">보관함</span>
+        </button>
+        <button className="sidebar__item">
+          <div className="sidebar__item-icon-wrapper">
+            <FontAwesomeIcon className="sidebar__item-icon" icon={faHistory} />
+          </div>
+          <span className="sidebar__item-text">최근 본 동영상</span>
+        </button>
+        <button className="sidebar__item">
+          <div className="sidebar__item-icon-wrapper">
+            <FontAwesomeIcon className="sidebar__item-icon" icon={faClock} />
+          </div>
+          <span className="sidebar__item-text">나중에 볼 동영상</span>
+        </button>
+        <button className="sidebar__item">
+          <div className="sidebar__item-icon-wrapper">
+            <FontAwesomeIcon className="sidebar__item-icon" icon={faThumbsUp} />
+          </div>
+          <span className="sidebar__item-text">좋아요 표시한 동영상</span>
+        </button>
+      </div>
+      <div className="sidebar__line"></div>
     </div>
   );
 };
